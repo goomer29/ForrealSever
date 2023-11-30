21c84494f1671c4683a931ed106b8f972071437a
User <User@LAB3-3>
2023-11-30 13:12:31 +0200

    finished

---
+++ dbo.Users.data.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[Users] ON
+INSERT INTO [dbo].[Users] ([ID], [Email], [UserName], [UserPswd]) VALUES (1, N'dark@goomer.com', N'darkgoomer', N'1234')
+INSERT INTO [dbo].[Users] ([ID], [Email], [UserName], [UserPswd]) VALUES (2, N'avig@gmail.cum', N'avigdor', N'1234569')
+SET IDENTITY_INSERT [dbo].[Users] OFF
