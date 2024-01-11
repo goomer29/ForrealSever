0bfb1a396a8a53e259ab5b5b146f24616bcf80ad
User <User@LAB3-3>
2024-01-11 12:29:59 +0200

    dfydydyd

---
+++ dbo.Challenges.data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT [dbo].[Challenges] ON
+INSERT INTO [dbo].[Challenges] ([ID], [Difficult], [Text]) VALUES (7, 1, N'say avigdormullet')
+INSERT INTO [dbo].[Challenges] ([ID], [Difficult], [Text]) VALUES (3, 2, N'tell a stupid joke to a friend')
+INSERT INTO [dbo].[Challenges] ([ID], [Difficult], [Text]) VALUES (4, 3, N'take a picture with someone with a yellow shirt')
+INSERT INTO [dbo].[Challenges] ([ID], [Difficult], [Text]) VALUES (5, 4, N'take a picture from the grabege mountain')
+INSERT INTO [dbo].[Challenges] ([ID], [Difficult], [Text]) VALUES (6, 4, N'do a backflip')
+
+SET IDENTITY_INSERT [dbo].[Challenges] OFF
